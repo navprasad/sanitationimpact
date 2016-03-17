ca9e0f3810b8669c24258e3a2aaf6359638de7d4
Prasanth P <prasanthxyz@gmail.com>
2016-03-17 15:06:31 +0530

    Requirements added

--- provider/models.py
+++ provider/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from manager.models import Manager
 
 
-class Toilet(models.Model):
-    toilet_id = models.CharField(max_length=100, unique=True)
-    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
-
-
 class Provider(models.Model):
     provider_id = models.CharField(max_length=100, unique=True)
     manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
+
+
+class Toilet(models.Model):
+    toilet_id = models.CharField(max_length=100, unique=True)
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
