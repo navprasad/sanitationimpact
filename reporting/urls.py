e676e8779d254ac6d00cc4e1a09b04981191be19
Prasanth P <prasanthxyz@gmail.com>
2016-04-10 04:25:07 +0530

    Is_Valid_Toilet
    - APIView which takes a toilet_id as POST and returns validity

--- reporting/urls.py
+++ reporting/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from reporting.views import RecordingViewSet, TicketViewSet
+from reporting.views import RecordingViewSet, TicketViewSet, IsValidToilet
 
 router = routers.DefaultRouter()
 router.register(r'recording', RecordingViewSet)
 router.register(r'ticket', TicketViewSet)
 
 urlpatterns = [
-    # url(r'^sample/', sample.view, name='sample_name'),
+    url(r'^is_valid_toilet/$', IsValidToilet.as_view(), name='is_valid_toilet'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- reporting/views.py
+++ reporting/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from administration.models import Toilet
 from reporting.models import Recording, Ticket
 from reporting.serializers import RecordingSerializer, TicketSerializer
 
@@ -12,3 +15,15 @@ class RecordingViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+
+class IsValidToilet(APIView):
+    def post(self, request):
+        toilet_id = request.data.get('toilet_id', None)
+        if not toilet_id:
+            return Response({'success': False, 'error': "Invalid POST data"})
+        try:
+            Toilet.objects.get(toilet_id=toilet_id)
+        except (Toilet.DoesNotExist, Toilet.DoesNotExist):
+            return Response({'success': False, 'error': "Invalid Toilet ID"})
+        return Response({'success': True})
