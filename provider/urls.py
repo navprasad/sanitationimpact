f44040cf5bdc4081c4695d4e71223bba133864ac
Prasanth P <prasanthxyz@gmail.com>
2016-04-10 15:13:10 +0530

    IsValidProvider and IsValidProviderPinCode APIView
    - APIView to check if input is a valid provider_id
    - APIView to check if input is a valid provider_id-pin_code combination

--- provider/urls.py
+++ provider/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from provider.views import ProviderViewSet
+from provider.views import IsValidProvider, IsValidProviderPinCode, ProviderViewSet
 
 router = routers.DefaultRouter()
 router.register(r'provider', ProviderViewSet)
 
 urlpatterns = [
-    # url(r'^sample/', sample.view, name='sample_name'),
+    url(r'^is_valid_provider/$', IsValidProvider.as_view(), name='is_valid_provider'),
+    url(r'^is_valid_provider_pin_code/$', IsValidProviderPinCode.as_view(), name='is_valid_provider_pin_code'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- provider/views.py
+++ provider/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from provider.models import Provider
 from provider.serializers import ProviderSerializer
@@ -7,3 +9,28 @@ from provider.serializers import ProviderSerializer
 class ProviderViewSet(viewsets.ModelViewSet):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+
+
+class IsValidProvider(APIView):
+    def post(self, request):
+        provider_id = request.data.get('provider_id', None)
+        if not provider_id:
+            return Response({'success': False, 'error': "Invalid POST data"})
+        try:
+            Provider.objects.get(provider_id=provider_id)
+        except (Provider.DoesNotExist, Provider.MultipleObjectsReturned):
+            return Response({'success': False, 'error': "Invalid Provider ID"})
+        return Response({'success': True})
+
+
+class IsValidProviderPinCode(APIView):
+    def post(self, request):
+        provider_id = request.data.get('provider_id', None)
+        pin_code = request.data.get('pin_code', None)
+        if not provider_id or not pin_code:
+            return Response({'success': False, 'error': "Invalid POST data"})
+        try:
+            Provider.objects.get(provider_id=provider_id, pin_code=pin_code)
+        except (Provider.DoesNotExist, Provider.MultipleObjectsReturned):
+            return Response({'success': False, 'error': "Invalid Provider ID/PIN Code"})
+        return Response({'success': True})
