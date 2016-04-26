e2c2ef1f8b29d8f6ba0a5b407000d8deddf3b1e5
Prasanth P <prasanthxyz@gmail.com>
2016-04-26 16:32:54 +0530

    Changed urls: prepended api/ to apis

--- provider/urls.py
+++ provider/urls.py
@@ -7,8 +7,9 @@ router = routers.DefaultRouter()
 router.register(r'provider', ProviderViewSet)
 
 urlpatterns = [
-    url(r'^is_valid_provider/$', IsValidProvider.as_view(), name='is_valid_provider'),
-    url(r'^is_valid_provider_pin_code/$', IsValidProviderPinCode.as_view(), name='is_valid_provider_pin_code'),
+    url(r'^api/is_valid_provider/$', IsValidProvider.as_view(), name='is_valid_provider'),
+    url(r'^api/is_valid_provider_pin_code/$', IsValidProviderPinCode.as_view(), name='is_valid_provider_pin_code'),
+
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- reporting/urls.py
+++ reporting/urls.py
@@ -9,10 +9,11 @@ router.register(r'recording', RecordingViewSet)
 router.register(r'ticket', TicketViewSet)
 
 urlpatterns = [
-    url(r'^is_valid_toilet/$', IsValidToilet.as_view(), name='is_valid_toilet'),
-    url(r'^is_valid_provider_ticket/$', IsValidProviderTicket.as_view(), name='is_valid_provider_ticket'),
-    url(r'^report_problem/$', ReportProblem.as_view(), name='report_problem'),
-    url(r'^report_fix/$', ReportFix.as_view(), name='report_fix'),
+    url(r'^api/is_valid_toilet/$', IsValidToilet.as_view(), name='is_valid_toilet'),
+    url(r'^api/is_valid_provider_ticket/$', IsValidProviderTicket.as_view(), name='is_valid_provider_ticket'),
+    url(r'^api/report_problem/$', ReportProblem.as_view(), name='report_problem'),
+    url(r'^api/report_fix/$', ReportFix.as_view(), name='report_fix'),
+
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
