82bdc953e2ba0f25e54fc511e35b3faadd2afb0f
Prasanth P <prasanthxyz@gmail.com>
2016-04-30 10:15:11 +0530

    Added the template files

--- admin/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from admin.models import Admin, Problem, ProblemCategory, Toilet
-
-admin.site.register(Admin)
-admin.site.register(Problem)
-admin.site.register(ProblemCategory)
-admin.site.register(Toilet)
